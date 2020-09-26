121897b2fe13f5a7227b3c84998cc8407f82ea3f
bkleiner <b.kleiner@str8labs.com>
2020-09-27 00:51:43 +0200

    blinky is a flash-dumper now

--- src/blinky/blinky.c
+++ src/blinky/blinky.c
@@ -1,18 +1,25 @@
 #include "clock.h"
 #include "delay.h"
+#include "flash.h"
 #include "led.h"
 #include "uart.h"
+
+static __xdata uint8_t buffer[1024];
 
 int main() {
   led_init();
 
-  //clock_init();
+  clock_init();
   uart_init();
 
   while (1) {
     led_red_toggle();
-    uart_put('!');
-    delay_ms(500);
-    led_green_toggle();
+    uint8_t magic = 0;
+    if (uart_get(&magic, 0x1FFF) && magic == '!') {
+      for (uint8_t i = 0; i < 16; i++) {
+        flash_read(i * 1024, buffer, 1024);
+        uart_write(buffer, 1024);
+      }
+    }
   }
 }
