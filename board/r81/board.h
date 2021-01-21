2aad3a357d2847b18a4a39faeb1b71616b21152d
bkleiner <b.kleiner@str8labs.com>
2021-01-21 20:37:32 +0100

    r81: find the first couple of pins

---
+++ board/r81/board.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#define UART_TX PA9
+
+#define CC2500_SCK_PIN PB3
+#define CC2500_MISO_PIN PB4
+#define CC2500_MOSI_PIN PB5
+
+#define CC2500_CSN_PIN PB0
+#define CC2500_GDO0_PIN PB1
+
+#define RF_TXEN PA1
+#define RF_RXEN PA2
+#define RF_MODE PA3
+#define RF_SWANT PA4
+
+#endif
