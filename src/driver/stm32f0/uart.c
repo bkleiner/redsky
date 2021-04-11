97b7cfdfaee5fc53a009a3473d25333cb2e7d3a5
bkleiner <b.kleiner@str8labs.com>
2021-04-11 03:19:37 +0200

    add sbus uart config for f0

--- src/driver/stm32f0/uart.c
+++ src/driver/stm32f0/uart.c
@@ -9,15 +9,34 @@ void uart_init() {
   gpio_config(UART_TX, GPIO_OUTPUT | GPIO_PP | GPIO_AF);
   gpio_config_af(UART_TX, 1);
 
+  // rx pin, not actually used
   gpio_config(UART_RX, GPIO_INPUT | GPIO_OD);
   gpio_config_af(UART_RX, 1);
 
+  // half duplex
+  //USART1->CR3 = USART_CR3_HDSEL;
   USART1->CR2 |= USART_CR2_TXINV | USART_CR2_RXINV;
-  USART1->CR1 = USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;
+
+#ifdef SERIAL_SBUS
+  // 1 parity bit
+  USART1->CR1 |= USART_CR1_M | USART_CR1_PCE;
+  // 2 stop bits
+  USART1->CR2 |= USART_CR2_STOP_1;
+  USART1->BRR = (SystemCoreClock / 200000);
+#endif
+
+#ifdef SERIAL_REDPINE
   USART1->BRR = (SystemCoreClock / 230400);
+#endif
+
+  // enable uart, enable transmission
+  USART1->CR1 |= USART_CR1_UE | USART_CR1_TE;
 }
 
 void uart_put(uint8_t val) {
+  USART1->CR1 &= ~USART_CR1_RE;
+  USART1->CR1 |= USART_CR1_TE;
+
   while (!(USART1->ISR & USART_ISR_TXE))
     ;
   USART1->TDR = val;
@@ -26,6 +45,9 @@ void uart_put(uint8_t val) {
 }
 
 uint8_t uart_get(uint8_t *val, uint16_t timeout) {
+  USART1->CR1 &= ~USART_CR1_TE;
+  USART1->CR1 |= USART_CR1_RE;
+
   while (!(USART1->ISR & USART_ISR_RXNE) && --timeout > 0)
     ;
 
