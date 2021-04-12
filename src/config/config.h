ebd5b1c47b30814672bcdcc0e9fac4490348ac58
bkleiner <b.kleiner@str8labs.com>
2021-04-12 16:50:11 +0200

    default procotol should be redpine

--- src/config/config.h
+++ src/config/config.h
@@ -6,10 +6,10 @@
 // #define DEBUG_OUTPUT
 // #define USE_FIXED_ANTENNA 1
 
-// #define PROTOCOL_FRSKY
 #define PROTOCOL_REDPINE
+// #define PROTOCOL_FRSKY
 
-// #define SERIAL_REDPINE
-#define SERIAL_SBUS
+#define SERIAL_REDPINE
+// #define SERIAL_SBUS
 
 #endif
